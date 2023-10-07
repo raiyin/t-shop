8c1c2cfcb709544df1e72bc42e672b4b2715e044
Maxim P <raiyin@ya.ru>
2023-10-07 20:47:07 +0300

    Header is done

--- t-shop/src/components/ShopHeader.vue
+++ t-shop/src/components/ShopHeader.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import ShopLogo from './icons/ShopLogo.vue';
-
-// export default {
-//     name: 'ShopHeader',
-// };
 </script>
 
 <template>
     <div class="header">
         <div class="header__logo"><ShopLogo /></div>
-        <div class="header__nav">links</div>
+        <div class="header__nav">
+            <ul>
+                <li class="nav__item-active">Главная</li>
+                <li>Корзина</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -17,6 +18,23 @@ import ShopLogo from './icons/ShopLogo.vue';
 .header {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     width: 100%;
+    padding: 0 20px;
+}
+
+li {
+    font-size: 14px;
+    display: block;
+    text-decoration: none;
+    float: left;
+}
+
+ul li:not(:last-child) {
+    margin-right: 24px;
+}
+
+.nav__item-active {
+    color: #ba967d;
 }
 </style>
