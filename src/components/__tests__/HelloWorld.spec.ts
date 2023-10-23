b5b585a91a36ca1902bc29d364e7aa06122264c7
Maxim P <raiyin@ya.ru>
2023-10-23 23:44:26 +0300

    Start unit testing

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,24 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import BasketItem from '../BasketItem.vue'
+import type { IProduct } from '@/model/IProduct'
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
+describe('BasketItem test', () => {
+
+    const product: IProduct = {
+        id: 1,
+        title: 'title',
+        price: '34',
+        old_price: 'old_price',
+        description: 'description',
+        category: 'category',
+        size: 'size',
+        count: 'count'
+    }
+    const wrapper = mount(BasketItem, { props: { product } })
+
+    it('should render properly', () => {
+        expect(wrapper.html()).toContain('18800')
+    })
 })
