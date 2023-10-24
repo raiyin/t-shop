25b609acdece7dbea8cc4a26588da89dd4e2632a
Maxim P <raiyin@ya.ru>
2023-10-24 23:36:43 +0300

    First syntetic test passed

--- src/components/BasketItem.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/vue'
-import { test } from '@jest/globals';
-import BasketItem from './BasketItem.vue'
-
-test('renders basket item', () => {
-    const { debug } = render(BasketItem)
-    debug()
-})
